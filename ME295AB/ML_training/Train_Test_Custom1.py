6a67c375b0a9463ef0074fb0fc1155ff2ead1c98
AmarisDLR <33881865+AmarisDLR@users.noreply.github.com>
2022-03-15 14:27:39 -0700

    Update Train_Test_Custom1.py

--- ME295AB/ML_training/Train_Test_Custom1.py
+++ ME295AB/ML_training/Train_Test_Custom1.py
@@ -19,7 +19,7 @@ import wandb
 from wandb.keras import WandbCallback
 
 
-wandb.init(project="ums3_custom_mar15", entity="amarisdlr")
+#wandb.init(project="ums3_custom_mar15", entity="amarisdlr")
 
 ### Work with pre-split data
 
@@ -54,43 +54,7 @@ def tensor_image_array(directory, input_shape,row_dim):
             n += 1
     return array_imgs
 
-'''def defectdetection(input_shape, lr):
-    #Initialize the model
-    #model = Sequential()
-      
-    # Layer 1
-    # Conv 1
-    model.add(Conv2D(filters=6, kernel_size=(5, 5), strides=1, activation = 'relu', input_shape = input_shape))
-    # Pooling 1
-    model.add(MaxPooling2D(pool_size=(2, 2), strides = 2))
-      
-    # Layer 2
-    # Conv 2
-    model.add(Conv2D(filters=16, kernel_size=(5, 5), strides=1, activation='relu'))
-    # Pooling 2
-    model.add(MaxPooling2D(pool_size = 2, strides = 2))
-  
-    # Flatten
-    model.add(Flatten())
-      
-    # Layer 3
-    # Fully connected layer 1
-    model.add(Dense(units=120, activation='relu'))
-      
-    #Layer 4
-    #Fully connected layer 2
-    model.add(Dense(units=84, activation='relu'))
-      
-    #Layer 5
-    #Output Layer
-    model.add(Dense(units=10, activation='softmax'))
-  
-    opt = SGD(lr = lr) # stochastic gradient descent method with learning rate lr = 0.01    
-    model.compile(optimizer=opt, 
-        loss='categorical_crossentropy',
-        metrics=['accuracy','categorical_accuracy','categorical_crossentropy'])
-    
-    return model'''
+
 #####################################
 
 shapehw = 32
@@ -189,12 +153,12 @@ model.compile(optimizer=opt,
 epochs = 75
 batch_size = 256
 
-wandb.config = {
+'''wandb.config = {
   "learning_rate": lr,
   "epochs": epochs,
   "batch_size": batch_size,
   "optimizer": opt
-}
+}'''
 
 # Start training
 start_train = time.time()
@@ -208,141 +172,14 @@ training_time = (end_train-start_train)/60
 
 timestr = time.strftime("%Y%m%d%H%M")
 history_version = timestr
-model.save('models/'+history_version+'.h5')
-'''
-timestr = time.strftime("%Y%m%d%H%M")
-history_version = timestr+'_tl_vgg16_'+str(shapehw)+'inshape_'+str(lr)+'LR_'+str(epochs)+'epochs_'+str(batch_size)+'batch'
-model.save('models/'+history_version+'.h5')
 
-#####################################
+savemodel = input("Save model? y/n")
 
-# Evaluate the accuracy and the loss in the test set
-scores = model.evaluate(valid_imgs, valid_labels, verbose=1)
-print("Scores: {}".format(scores))
-scores0 = "%.2f" % scores[0]
-scores1 = "%.2f" % scores[1]
-
-#####################################
-
-# Retrieve a list of accuracy results on training and validation data
-# sets for each training epoch
-acc = history.history['accuracy']
-val_acc = history.history['val_accuracy']
-
-# Retrieve a list of list results on training and validation data
-# sets for each training epoch
-loss = history.history['loss']
-val_loss = history.history['val_loss']
-
-
-subimg= valid_dir+'/stringing/1634702965_UMS3_hexagon_squareoval_15infill_lines_9911img_jpg.rf.44e8c67f064a0d4c792dcd9860d5cb90.jpg'
-img = cv2.imread(subimg)
-img_tensor = torch.from_numpy(img)/255.0
-img_tensor = img_tensor.resize_(input_shape)
-img_tensor = img_tensor.unsqueeze(dim=0)
-img_tensor = img_tensor.numpy()
-
-prediction = model.predict(img_tensor)
-print('Actually: overextrusion; Prediction: {}'.format(prediction))
-
-savefigs = 'y'# input('Save figs? (y/n) ')
-
-if savefigs=='y':
-	# Get number of epochs
-	epochs = range(len(acc))
-
-	# Plot training and validation accuracy per epoch
-	plt.plot(epochs, acc)
-	plt.plot(epochs, val_acc)
-	plt.legend(["Accuracy", "Val_Accuracy"])
-	plt.xlabel("Epochs")
-	plt.ylabel("Accuracy")
-	plt.title('Training and validation accuracy: '+'Val Loss='+scores0 +' Val Accuracy='+scores1)
-	plt.savefig('models/'+history_version+'_train_val_accuracy.jpg')
-
-	# Plot training and validation loss per epoch
-	plt.figure()
-	plt.plot(epochs, loss)
-	plt.plot(epochs, val_loss)
-	plt.legend(["Loss", "Val_Loss"])
-	plt.xlabel("Epochs")
-	plt.ylabel("Loss")
-	plt.title('Training and validation loss: '+'Val Loss='+scores0+' Val Accuracy='+scores1)
-	plt.savefig('models/'+history_version+'_train_val_loss.jpg')
-	print('Plots saved.')
+if savemodel == 'y' or savemodel == "Y":
+	model.save('models/'+history_version+'.h5')
+	print("Model saved.")
 else:
-	print('No plots saved.\n')
-print('Program completed.\n')
-print("Training took: "+str(training_time)+" min.")
-'''
-
-'''
-# In[ ]:
-
-
-for root, subdir, files in os.walk(train_dir):
-    for subfolder in subdir:
-        print(subfolder)
-
-
-# In[1]:
-
-
-import os
-from os.path import exists
-import cv2
-import torch
-import numpy as np
-import time
-import matplotlib.pyplot as plt
-
-import tensorflow as tf
-
-
-from tensorflow import keras
-tic = time.time()
-model = keras.models.load_model('UMS3_0/202111222308_tl_vgg16_0.55dropout_32inshape_0.0001LR_75epochs_256batch.h5')
-toc = time.time()
-toc-tic
-
-
-# In[20]:
-
-
-shapehw = 32
-input_shape = (shapehw,shapehw,3)
-
-base_dir = 'UMS3_0'
-train_dir = os.path.join(base_dir,'train')
-valid_dir = os.path.join(base_dir,'valid')
-subimg= valid_dir+'/stringy/1634329033_34638img_jpg.rf.f2c682789e67e01b3b663f2c2329892b.jpg'
-
-
-# In[26]:
-
-
-subimg= '../../UMS3_project/ME295AB/split34_gcodeoverlay.jpg'
-shapehw = 32
-input_shape = (shapehw,shapehw,3)
-img = cv2.imread(subimg)
-img_tensor = torch.from_numpy(img)
-img_tensor = img_tensor.resize_(input_shape)/255
-img_tensor = img_tensor.unsqueeze(dim=0)
-img_tensor = img_tensor.numpy()
-
-prediction = model.predict(img_tensor)
-prediction
-
-
-# In[14]:
-
-
-classes = list(np.argmax(prediction, axis=1))
-classes
-
-
-# In[ ]:
-
-
-'''
+	pass
+print("End training")
+	
 
